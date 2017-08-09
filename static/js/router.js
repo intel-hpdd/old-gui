bb37a394cd80ff1c9c0273b58878498e16505f69
Joe Grund <joe.grund@intel.com>
2017-08-09 12:26:23 -0400

    Remove all old storage code + highcharts. (#9)
    Remove all the storage page code in this repo.
    
    In addition, remove highcharts + related code.
    
    Signed-off-by: Joe Grund <joe.grund@intel.com>

--- static/js/router.js
+++ static/js/router.js
@@ -20,10 +20,9 @@
 // express and approved by Intel in writing.
 
 
-var RouteUtils = function() {
+var RouteUtils = function () {
 
-  function api_path_to_ui_path(resource_uri)
-  {
+  function api_path_to_ui_path(resource_uri) {
     /* Given an API resource URI for an object,
      return the UI URI for the detail view */
     var resource = resource_uri.split('/')[2];
@@ -34,22 +33,20 @@ var RouteUtils = function() {
     } else if (resource == 'host') {
       return "/configure/server/" + id + "/";
     } else if (resource == 'storage_resource') {
-      return "/configure/storage/" + id + "/";
+      return `/configure/storage/${id}/`;
     } else {
       return "/" + resource + "/" + id + "/";
     }
   }
 
-  function detail_path_to_list_path(detail_uri)
-  {
+  function detail_path_to_list_path(detail_uri) {
     /* Given a detail URI for an object, return the
      list URI for that object type */
     var resource = detail_uri.split('/')[2];
 
     var resource_to_list_uri = {
       "volume": "/configureold/volume/",
-      "user": "/configureold/user/",
-      "storage_resource": "/configureold/storage/"
+      "user": "/configureold/user/"
     };
 
     /* FIXME: can't do the mapping for a /target/ URI because
@@ -65,8 +62,7 @@ var RouteUtils = function() {
     return "/" + window.location.pathname.substr(UI_ROOT.length);
   }
 
-  function uri_properties_link(resource_uri, label)
-  {
+  function uri_properties_link(resource_uri, label) {
     if (resource_uri) {
       var url = api_path_to_ui_path(resource_uri);
 
@@ -76,8 +72,7 @@ var RouteUtils = function() {
     }
   }
 
-  function object_properties_link(object, label)
-  {
+  function object_properties_link(object, label) {
     if (!label) {
       label = LiveObject.label(object);
     }
@@ -92,7 +87,7 @@ var RouteUtils = function() {
       return results[1];
   }
 
-  function detail_page (a, b, c) {
+  function detail_page(a, b, c) {
     return '<a class="navigation" href="/ui/%s/%s">%s</a>'
       .sprintf(a, b, c);
   }
@@ -108,24 +103,23 @@ var RouteUtils = function() {
   }
 }();
 
-  var routes = {
-    'configureold/filesystem/detail/:id/': 'filesystemDetail',
-    'configureold/filesystem/create/': 'filesystemCreate',
-    'configureold/:tab/': 'configure',
-    'configureold/': 'configureIndex',
-    'targetold/:id/': 'target_detail',
-    'userold/:id/': 'user_detail',
-    'storage_resourceold/:id/': 'storage_resource_detail',
-    'system_statusold/': 'system_status'
-  };
+var routes = {
+  'configureold/filesystem/detail/:id/': 'filesystemDetail',
+  'configureold/filesystem/create/': 'filesystemCreate',
+  'configureold/:tab/': 'configure',
+  'configureold/': 'configureIndex',
+  'targetold/:id/': 'target_detail',
+  'userold/:id/': 'user_detail',
+  'system_statusold/': 'system_status'
+};
 
-  routes = Object.keys(routes).reduce(function (newRoutes, route) {
-    'use strict';
+routes = Object.keys(routes).reduce(function (newRoutes, route) {
+  'use strict';
 
-    newRoutes[route] = newRoutes[route.slice(0, -1)] = routes[route];
+  newRoutes[route] = newRoutes[route.slice(0, -1)] = routes[route];
 
-    return newRoutes;
-  }, {});
+  return newRoutes;
+}, {});
 
 
 
@@ -138,44 +132,46 @@ var RouteUtils = function() {
 var ChromaRouter = Backbone.Router.extend({
   routes: routes,
   conf_param_dialog: null,
-  object_detail: function(id, model_class, view_class, title_attr, overridePropertiesFunc)
-  {
-    var c = new model_class({id: id});
-    c.fetch({success: function(model, response) {
-      // Remove existing dialog if present, create a new one.
-      var dialog_id = view_class.prototype.className + "_" + id;
-      $('#' + dialog_id).remove();
-      var mydiv = $("<div id='" + dialog_id + "' style='overflow-y: scroll;'></div>");
+  object_detail: function (id, model_class, view_class, title_attr, overridePropertiesFunc) {
+    var c = new model_class({ id: id });
+    c.fetch({
+      success: function (model, response) {
+        // Remove existing dialog if present, create a new one.
+        var dialog_id = view_class.prototype.className + "_" + id;
+        $('#' + dialog_id).remove();
+        var mydiv = $("<div id='" + dialog_id + "' style='overflow-y: scroll;'></div>");
 
-      var title = (_.isFunction(title_attr)? title_attr(c): c.get(title_attr));
+        var title = (_.isFunction(title_attr) ? title_attr(c) : c.get(title_attr));
 
-      var properties = {
-        buttons: [
-          {text: "Close", 'class': "close", click: function () {
-            $(this).dialog('close');
-          }}
-        ],
-        close: function (event, ui) {
-          window.history.back();
-          $(this).dialog('destroy').remove();
-        },
-        width: 600,
-        height: 600,
-        modal: true,
-        title: title
-      };
+        var properties = {
+          buttons: [
+            {
+              text: "Close", 'class': "close", click: function () {
+                $(this).dialog('close');
+              }
+            }
+          ],
+          close: function (event, ui) {
+            window.history.back();
+            $(this).dialog('destroy').remove();
+          },
+          width: 600,
+          height: 600,
+          modal: true,
+          title: title
+        };
 
-      properties = (_.isFunction(overridePropertiesFunc)?
-        overridePropertiesFunc.bind(null, c): _.identity
-      )(properties);
+        properties = (_.isFunction(overridePropertiesFunc) ?
+          overridePropertiesFunc.bind(null, c) : _.identity
+        )(properties);
 
-      mydiv.dialog(properties);
-      var cd = new view_class({model: c, el: mydiv.parent()});
-      cd.render();
-    }})
+        mydiv.dialog(properties);
+        var cd = new view_class({ model: c, el: mydiv.parent() });
+        cd.render();
+      }
+    })
   },
-  target_detail: function(id)
-  {
+  target_detail: function (id) {
     this.object_detail(id, Target, TargetDetail, function titleFunc(c) {
       var kinds = {
         MDT: 'Metadata Target',
@@ -188,44 +184,37 @@ var ChromaRouter = Backbone.Router.extend({
 
       return '%s: %s'.sprintf(kind, label);
     },
-    function propertyFunc(c, properties) {
-      if (c.get('kind') === 'OST') {
-        properties.height = 710;
-        properties.open = function () {
-          $(this).css('overflow', 'hidden');
+      function propertyFunc(c, properties) {
+        if (c.get('kind') === 'OST') {
+          properties.height = 710;
+          properties.open = function () {
+            $(this).css('overflow', 'hidden');
+          }
         }
-      }
 
-      return properties;
-    });
+        return properties;
+      });
   },
-  storage_resource_detail: function(id)
-  {
-    this.object_detail(id, StorageResource, StorageResourceDetail, 'class_name');
-  },
-  user_detail: function(id)
-  {
+  user_detail: function (id) {
     this.object_detail(id, User, UserDetail, 'username');
   },
-  system_status:function() {
+  system_status: function () {
     this.toplevel('system_status');
     (new SystemStatusView()).render();
   },
-  failed_filesystem_admin_check: function() {
-    if ( Login.userHasGroup('filesystem_administrators') )
+  failed_filesystem_admin_check: function () {
+    if (Login.userHasGroup('filesystem_administrators'))
       return false;
 
     window.location.href = '%sdashboard/'.sprintf(Api.UI_ROOT);
   },
-  configureIndex: function()
-  {
-    if ( this.failed_filesystem_admin_check() )
+  configureIndex: function () {
+    if (this.failed_filesystem_admin_check())
       return;
 
     this.configure('filesystem');
   },
-  toplevel: function(name, aroundDatetime)
-  {
+  toplevel: function (name, aroundDatetime) {
     $('div.toplevel').hide();
     $("#toplevel-" + name).show();
 
@@ -246,8 +235,8 @@ var ChromaRouter = Backbone.Router.extend({
 
     nextTab.show();
   },
-  configure: function(tab) {
-    if ( this.failed_filesystem_admin_check() )
+  configure: function (tab) {
+    if (this.failed_filesystem_admin_check())
       return;
 
     this.configureTab(tab);
@@ -256,8 +245,6 @@ var ChromaRouter = Backbone.Router.extend({
       VolumeView.draw()
     } else if (tab == 'user') {
       UserView.draw()
-    } else if (tab == 'storage') {
-      StorageView.draw()
     } else if (tab == 'mgt') {
       MgtView.draw()
     } else if (tab === 'power') {
@@ -279,20 +266,20 @@ var ChromaRouter = Backbone.Router.extend({
       });
     }
   },
-  filesystemPage: function(page) {
+  filesystemPage: function (page) {
     this.configureTab('filesystem');
     $('#filesystem-tab-create').hide();
     $('#filesystem-tab-detail').hide();
     $('#filesystem-tab-' + page).show();
   },
-  filesystemDetail: function(id) {
-    if ( this.failed_filesystem_admin_check() )
+  filesystemDetail: function (id) {
+    if (this.failed_filesystem_admin_check())
       return;
     this.filesystemPage('detail');
-    FilesystemDetailView.draw(id,this)
+    FilesystemDetailView.draw(id, this)
   },
-  filesystemCreate: function() {
-    if ( this.failed_filesystem_admin_check() )
+  filesystemCreate: function () {
+    if (this.failed_filesystem_admin_check())
       return;
     this.filesystemPage('create');
     FilesystemCreateView.draw(this)
