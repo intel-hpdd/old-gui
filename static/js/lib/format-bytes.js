0c6f3e7a2e2b46cfdaac363a5ec8672e46f684ff
Will Johnson <wjohnson@whamcloud.com>
2019-02-14 17:46:52 -0500

    Filesystem Detail Chart shows incorrect units for storage statistics
    Fixes #22.
    
    The storage metrics on the filesystem detail page and targets should be calculated using the number-formatters algorithm. Unfortunately, the old gui will not allow the repo to be included and bundled, so for now I think it makes sense to replace the functionality in misc.js with the logic in number-formatters.
    
    Signed-off-by: Will Johnson <wjohnson@whamcloud.com>

---
+++ static/js/lib/format-bytes.js
@@ -0,0 +1,18 @@
+//
+// Copyright (c) 2019 DDN. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+function formatBytes (bytes, precision) {
+  const units = ['B', 'kiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+
+  if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '';
+  precision = precision || 4;
+
+  bytes = Math.max(bytes, 0);
+  let pwr = Math.floor(Math.log(bytes) / Math.log(1024));
+  pwr = Math.min(pwr, units.length - 1);
+  pwr = Math.max(pwr, 0);
+  bytes /= Math.pow(1024, pwr);
+  return `${bytes.toPrecision(precision)} ${units[pwr]}`;
+};
